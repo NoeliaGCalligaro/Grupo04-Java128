0e840cd8f17b09b2a766f51eb5bd177f9147cc09
NoeliaGCalligaro <106279844+NoeliaGCalligaro@users.noreply.github.com>
2023-03-30 08:51:53 -0300

    Ultimos cambios-error

--- TrabajoEntrega01/src/TrabajoPracticoEntrega01/PronosticoPartidos.java
+++ TrabajoEntrega01/src/TrabajoPracticoEntrega01/PronosticoPartidos.java
@@ -1,11 +1,13 @@
 
 package TrabajoPracticoEntrega01;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+import java.util.Scanner;
 
 public class PronosticoPartidos {
 
@@ -17,7 +19,7 @@ public class PronosticoPartidos {
 	static int puntos2=0;
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		
 		//Leer-Cargar los datos de los partidos en el objeto partido
 		
@@ -42,12 +44,10 @@ public class PronosticoPartidos {
 			
 		//leer / cargar a los jugadores en la clase jugador 
 			
-		//comparamos los resultados con los pronosticos por cada jugador
+			//comparamos los resultados con los pronosticos por cada jugador
 		//mostramos el puntaje
 			
-		//comparar los puntajes de los jugadores para que los muestre ordenados.
-		
-		
+			//comparar los puntajes de los jugadores para que los muestre ordenados.
 		//compararResultados();
 
 	}
@@ -56,27 +56,30 @@ public class PronosticoPartidos {
 	
 
 
-	private static void leerPronosticos() {
-		Path rutaPronosticos = Paths.get("src\\TrabajoPracticoEntrega01\\Pronosticos.txt");
-
-		
-		
-		try {
-	         for(String linea : Files.readAllLines(rutaPronosticos)) { 
-	        	    String jugador =linea.split(" ")[0]; 
-	        	    String equipo =linea.split(" ")[1]; 
-					String resultado =linea.split(" ")[2]; 
-					Pronostico pronostico=new Pronostico(equipo, resultado, jugador);
-					pronosticos.add(pronostico);
-					
-					
-					
-					}
-
-	         }catch (IOException e) {
-	 			
-	 			e.printStackTrace();
-	 		}
+	private static void leerPronosticos() throws FileNotFoundException {
+		String rutaPronosticos = "/home/noelia/GitHub/Grupo04-Java128/TrabajoEntrega01/src/TrabajoPracticoEntrega01/Pronosticos.txt";
+		  
+			
+			File file = new File(rutaPronosticos);
+			  try (Scanner scanner = new Scanner(file)) {            
+				  while (scanner.hasNext()) {
+                String linea = scanner.nextLine();
+                System.out.println(linea);
+                String jugador =linea.split(" ")[0]; 
+        	    String equipo =linea.split(" ")[1]; 
+        	 
+				String resultado =linea.split(" ")[2]; 
+			 Pronostico pronostico=new Pronostico(equipo, resultado, jugador);
+				pronosticos.add(pronostico);
+              
+            }
+            scanner.close();
+		 }catch (NumberFormatException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		        
+	        
 	 	
 		pronosticos.forEach(System.out::println); 
 
@@ -90,7 +93,7 @@ public class PronosticoPartidos {
 	while(ite.hasNext()) {
 		Partido partido=(Partido) ite.next();
 		if(partido.getResultado1()>partido.getResultado2()) {
-			Iterator<Resultado> iter= resultados.iterator();
+			Iterator iter= resultados.iterator();
 			while(iter.hasNext()) {
 				Resultado resultado=(Resultado) ite.next();
 				resultado.setResultado("Gana");
@@ -106,7 +109,7 @@ public class PronosticoPartidos {
 		     
 	}
 }else if(partido.getResultado1()==partido.getResultado2()) {
-	Iterator<Resultado> iter= resultados.iterator();
+	Iterator iter= resultados.iterator();
 	while(iter.hasNext()) {
 		Resultado resultado=(Resultado) ite.next();
 		resultado.setResultado("Empate");
@@ -119,23 +122,30 @@ public class PronosticoPartidos {
 	}
 
 
-	private static void cargarPartidos() {
-		Path rutaResultados = Paths.get("src\\TrabajoPracticoEntrega01\\Resultados.txt");
-            try {
-		         for(String linea : Files.readAllLines(rutaResultados)) { 
-		        	 String equipo1 =linea.split(" ")[0]; 
-					    int resultados1 =Integer.parseInt(linea.split(" ")[1]); 
-						int resultados2 =Integer.parseInt(linea.split(" ")[2]);
-						String equipo2 =linea.split(" ")[3]; 
-						Partido partido =new Partido(equipo1, equipo2,resultados1,resultados2);
-						partidos.add(partido);
-						
-						}
+	private static void cargarPartidos() throws FileNotFoundException {
+		
+		String rutaResultados= "/home/noelia/GitHub/Grupo04-Java128/TrabajoEntrega01/src/TrabajoPracticoEntrega01/Resultados.txt";
+		
 
-		         }catch (IOException e) {
-		 			
-		 			e.printStackTrace();
-		 		}
+		File file = new File(rutaResultados);
+        try (Scanner scanner = new Scanner(file)) {
+			while (scanner.hasNext()) {
+			   String linea = scanner.nextLine();
+			   String equipo1 =linea.split(" ")[0]; 
+			    int resultados1 =Integer.parseInt(linea.split(" ")[1]); 
+			    System.out.println(linea);
+				int resultados2 =Integer.parseInt(linea.split(" ")[2]);
+				String equipo2 =linea.split(" ")[3]; 
+				Partido partido =new Partido(equipo1, equipo2,resultados1,resultados2);
+				partidos.add(partido);
+			  
+			}
+			scanner.close();
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
 		 		
     
 	}
