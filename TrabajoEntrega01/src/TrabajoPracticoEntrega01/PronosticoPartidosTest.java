5dd6183c61d3f1a8c541a5b8f59f72b164fb03fe
NoeliaGCalligaro <106279844+NoeliaGCalligaro@users.noreply.github.com>
2023-04-01 21:52:05 -0300

    Test

---
+++ TrabajoEntrega01/src/TrabajoPracticoEntrega01/PronosticoPartidosTest.java
@@ -0,0 +1,14 @@
+package TrabajoPracticoEntrega01;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PronosticoPartidosTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
